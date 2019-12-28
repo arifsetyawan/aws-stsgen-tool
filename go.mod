85efa7e894a6f8abdbf4d97a70c37e2407004f27
Arif Setyawan <me@arifsetyawan.com>
2019-12-28 23:22:31 +0800

    completing base functionalities install and set

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/arifsetyawan/aws-stsgen-tool
 
 go 1.13
+
+require (
+	github.com/manifoldco/promptui v0.6.0
+	github.com/spf13/viper v1.6.1 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/spf13/viper"
 	"io/ioutil"
 	"os"
 	"os/exec"
@@ -13,7 +14,7 @@ func main() {
 
 	var theRes map[string]interface{}
 
-	command, value, errArgs := argsh()
+	command, errArgs := argsh()
 	if errArgs != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", errArgs)
 		os.Exit(1)
@@ -21,9 +22,32 @@ func main() {
 
 	switch command {
 	case "install":
-		installSerialNumber(value)
+		install()
 	case "set":
-		result, err := requestStsCredential(value)
+
+		// Get Current User
+		usr, err := user.Current()
+		if err != nil {
+			fmt.Errorf("%v", err)
+		}
+
+		viper.SetConfigName("config") // name of config file (without extension)
+		viper.AddConfigPath(usr.HomeDir+"/.awsstsgen/")
+		err = viper.ReadInConfig()
+		if err != nil {
+			panic(fmt.Errorf("Fatal error config file: %s \n", err))
+		}
+
+		var mfaToken string
+
+		// Get token prompt
+		fmt.Print("Please input current MFA token: ")
+		_, err = fmt.Scanln(&mfaToken)
+		if err != nil {
+			panic("Require MFA Token")
+		}
+
+		result, err := requestStsCredential(mfaToken, viper.Get("mfa-arn").(string), viper.Get("base-profile").(string))
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "error: %v\n", err)
 			os.Exit(1)
@@ -36,38 +60,36 @@ func main() {
 		secretKey := credentials["SecretAccessKey"].(string)
 		sessionToken := credentials["SessionToken"].(string)
 
-		profile := "cred-mfa"
+		profile := viper.Get("target-profile").(string)
 
-		fmt.Println(credentials)
+		fmt.Println("accessKey: "+accessKey)
+		fmt.Println("secretKey: "+secretKey)
+		fmt.Println("sessionToken: "+sessionToken)
 
-		configureLocalCredential(accessKey, secretKey, sessionToken, profile)
+		if configureLocalCredential(accessKey, secretKey, sessionToken, profile) == true {
+			fmt.Println("\nCREDENTIAL UPDATED")
+		}
 	}
 
 }
 
+func argsh() (string, error) {
 
-func argsh() (string, string, error) {
-	if len(os.Args) < 3 {
-		return "", "", fmt.Errorf("%s", "Incomplete Command")
+	if len(os.Args) < 2 {
+		return "", fmt.Errorf("%s", "Incomplete Command")
 	}
+
 	command := os.Args[1:][0]
-	value := os.Args[1:][1]
-
-	fmt.Println(command)
-	fmt.Println(value)
-
-	return command, value, nil
+	return command, nil
 }
 
-func requestStsCredential(token string) (interface{}, error) {
+func requestStsCredential(token string, serial string, baseProfile string) (interface{}, error) {
 	binary, lookErr := exec.LookPath("aws")
 	if lookErr != nil {
 		return nil, lookErr
 	}
 
-	serial_id := readSerialNumber()
-
-	cmd := exec.Command(binary, "sts", "get-session-token", "--serial-number", serial_id, "--token-code", token, "--duration-seconds", "129600", "--profile", "wallex")
+	cmd := exec.Command(binary, "sts", "get-session-token", "--serial-number", serial, "--token-code", token, "--duration-seconds", "129600", "--profile", baseProfile)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		return nil, err
@@ -76,51 +98,71 @@ func requestStsCredential(token string) (interface{}, error) {
 }
 
 func configureLocalCredential(accessKey string, secretKey string, sessionToken string, profile string) bool {
-
 	binary, lookErr := exec.LookPath("aws")
 	if lookErr != nil {
 		return false
 	}
-	exec.Command(binary, "configure", "set", "aws_access_key_id", accessKey, "--profile", profile).CombinedOutput()
-	exec.Command(binary, "configure", "set", "aws_secret_access_key", secretKey, "--profile", profile).CombinedOutput()
-	exec.Command(binary, "configure", "set", "aws_session_token", sessionToken, "--profile", profile).CombinedOutput()
-
-	return true
-
-}
-
-func installSerialNumber(value string) bool {
-
-	usr, err := user.Current()
+	_, err := exec.Command(binary, "configure", "set", "aws_access_key_id", accessKey, "--profile", profile).CombinedOutput()
+	_, err = exec.Command(binary, "configure", "set", "aws_secret_access_key", secretKey, "--profile", profile).CombinedOutput()
+	_, err = exec.Command(binary, "configure", "set", "aws_session_token", sessionToken, "--profile", profile).CombinedOutput()
 	if err != nil {
+		fmt.Errorf("Error Execute aws configure ", err)
 		return false
 	}
 
-	d1 := []byte(value)
+	return true
+}
 
+func install() {
+
+	var resultArn, resultBaseProfile, resultTargetProfile string
+
+	fmt.Print("Your mfa arn : ")
+	_, err := fmt.Scanln(&resultArn)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	fmt.Print("Your base credential name in ~/.aws/credential (default): ")
+	_, err = fmt.Scanln(&resultBaseProfile)
+	if err != nil {
+		resultBaseProfile = "default"
+	}
+
+	fmt.Print("Your target credential name (default-mfa): ")
+	_, err = fmt.Scanln(&resultTargetProfile)
+	if err != nil {
+		resultTargetProfile = "default-mfa"
+	}
+
+	preparedConfig := map[string]interface{}{
+		"mfa-arn": resultArn,
+		"base-profile": resultBaseProfile,
+		"target-profile": resultTargetProfile,
+	}
+
+	preparedConfigData, _ := json.MarshalIndent(preparedConfig, "", " ")
+
+	// Get Current User
+	usr, err := user.Current()
+	if err != nil {
+		fmt.Errorf("%v", err)
+	}
+
+	// Checking if the .awsstsgen is exist, otherwise create it.
 	if _, err := os.Stat(usr.HomeDir+"/.awsstsgen/"); os.IsNotExist(err) {
-		os.Mkdir(usr.HomeDir+"/.awsstsgen", 0777)
+		_ = os.Mkdir(usr.HomeDir+"/.awsstsgen", 0777)
 	}
 
-	f, err := os.Create(usr.HomeDir+"/.awsstsgen/.serial_id")
-	defer f.Close()
-	n2, err := f.Write(d1)
-	fmt.Printf("wrote %d bytes\n", n2)
-	if err != nil {
-		fmt.Println(err)
-		return false
-	}
-	return true
-}
-
-func readSerialNumber() string {
-	usr, err := user.Current()
-	if err != nil {
-		return ""
+	// save config file
+	errSaveConfig := ioutil.WriteFile(usr.HomeDir+"/.awsstsgen/config.json", preparedConfigData, 0644)
+	if errSaveConfig != nil {
+		fmt.Errorf("%v", errSaveConfig)
+		panic(errSaveConfig)
 	}
 
-	dat, err := ioutil.ReadFile(usr.HomeDir+"/.awsstsgen/.serial_id")
-	fmt.Print(string(dat))
+	fmt.Println("\n=======================================================================")
+	fmt.Println("Install complete!\nyou can check your config at ~/.awsstsgen/config.json")
 
-	return string(dat)
 }
